b91d9ad91d63e03a3b6c9e1e6e5364730afb9f01
Marcos Ribas <contatomarcosribas@gmail.com>
2020-12-22 19:39:33 -0300

    Java001

---
+++ Java/ex001/src/ex001/Ex001.java
@@ -0,0 +1,11 @@
+/*Ex001 Crie um programa que escreva “Olá, Mundo!” na tela.
+*/
+package ex001;
+
+public class Ex001 {
+
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!");
+    }
+    
+}
