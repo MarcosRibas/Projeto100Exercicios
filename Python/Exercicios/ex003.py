4a162d820fd3a107167d5e8f0410b123fadfb2aa
Marcos Ribas <contatomarcosribas@gmail.com>
2020-12-22 19:29:18 -0300

    exercicios de 1 a 5 python

---
+++ Python/Exercicios/ex003.py
@@ -0,0 +1,5 @@
+#Ex003 Crie um programa que leia dois números e mostre a soma entre eles.
+n1 = int(input('Digite um número: '))
+n2 = int(input('Agora digite outro número: '))
+soma = n1 + n2
+print("A soma entre", n1, "e", n2, "é: ", soma)
---
+++ Python/Exercicios/ex005.py
@@ -0,0 +1,5 @@
+#Ex005 Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+n = int(input('Digite um número inteiro: '))
+a = n - 1
+s = n + 1
+print('O número que você digitou é', n, 'tendo seu antecessor', a, 'e seu sucessor', s)
