2ea6aa767216b8aedbb78909479a777dc847434f
Marcos Ribas <74434215+MarcosRibas@users.noreply.github.com>
2020-12-26 12:11:07 -0300

    Update ex005.py

--- Python/ex005.py
+++ Python/ex005.py
@@ -2,4 +2,4 @@
 n = int(input('Digite um número inteiro: '))
 a = n - 1
 s = n + 1
-print('O número que você digitou é', n, 'tendo seu antecessor', a, 'e seu sucessor', s)
+print('O número que você digitou é {} tendo seu antecessor {} e seu sucessor {}'.format(n, a, s))
