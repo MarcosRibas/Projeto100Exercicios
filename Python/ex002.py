ef9f4b88da600b736c43729627cacc8be0997ff8
Marcos Ribas <74434215+MarcosRibas@users.noreply.github.com>
2020-12-26 12:07:14 -0300

    Update ex002.py

--- Python/ex002.py
+++ Python/ex002.py
@@ -1,3 +1,3 @@
 #Ex002 Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
 nome = input('Digite seu nome: ')
-print('Olá, ', nome, ' Seja bem vindo!')
+print('Olá, {}. Seja bem vindo!'.format(nome))
