79b682fbfb9a2440b62d7243ffd6be1900a3dc0a
Marcos Ribas <contatomarcosribas@gmail.com>
2021-01-06 19:58:34 -0300

    Java

---
+++ Java/Ex050.java
@@ -0,0 +1,21 @@
+/*
+Ex050 Desenvolva um programa que leia seis números inteiros e mostre a soma 
+apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+ */
+package ex050;
+
+import java.util.Scanner;
+
+public class Ex050 {
+
+    public static void main(String[] args) {
+        int par = 0;
+        Scanner t = new Scanner(System.in);
+        for (int c = 0; c <= 6; c++)
+            System.out.println("Digite um valor?");
+            int num = t.nextInt();
+        
+
+    
+    
+}}
