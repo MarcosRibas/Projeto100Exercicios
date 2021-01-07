7d75624cb20241ea216893f7d142fcf77e45cc0b
Marcos Ribas <74434215+MarcosRibas@users.noreply.github.com>
2021-01-07 20:14:46 -0300

    Update ex010.py

--- Python/ex010.py
+++ Python/ex010.py
@@ -1,5 +1,5 @@
-#Ex010 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre
-# quantos Dólares ela pode comprar. Considere o dólar atual
+'''Ex010 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre
+quantos Dólares ela pode comprar. Considere o dólar atual'''
 real = float(input('Quantos reais você tem? R$'))
 dolar = real / 5.22
 print('Seu dinheiro convertido em doláres fica em: US${:.2f}'.format(dolar))
