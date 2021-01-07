0b8f58982eb120f4f25e7f0c1485dd97b0b1b53e
Marcos Ribas <74434215+MarcosRibas@users.noreply.github.com>
2021-01-07 20:11:19 -0300

    Update ex004.py

--- Python/ex004.py
+++ Python/ex004.py
@@ -1,5 +1,5 @@
-#Ex004 Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e
-# todas as informações possíveis sobre ele.
+'''Ex004 Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e
+todas as informações possíveis sobre ele.'''
 a = input('Digite algo: ')
 print('É um número?', a.isnumeric())
 print('É alfabético?', a.isalpha())
