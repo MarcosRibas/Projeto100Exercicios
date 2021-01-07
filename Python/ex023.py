02059981fefdb07a17757b73139904b2ad9d29dc
Marcos Ribas <contatomarcosribas@gmail.com>
2021-01-07 20:37:25 -0300

    Python

--- Python/ex023.py
+++ Python/ex023.py
@@ -1,9 +1,9 @@
-#Ex023 Faça um programa que leia um número de 1 a 9999 e mostre na tela cada um dos dígitos separados.
-#exemplo: digite um número: 1843
-#Unidade: 4
-#Dezena: 3
-#Centena: 8
-#Milhar: 1
+'''Ex023 Faça um programa que leia um número de 1 a 9999 e mostre na tela cada um dos dígitos separados.
+exemplo: digite um número: 1843
+Unidade: 4
+Dezena: 3
+Centena: 8
+Milhar: 1'''
 
 n = int(input('Digite um número de 1 a 9999: '))
 u = n // 1 % 10
