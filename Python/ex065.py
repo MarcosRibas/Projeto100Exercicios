e937ac414b107c228ef747a7cd012c283028073b
Marcos Ribas <contatomarcosribas@gmail.com>
2021-01-08 21:48:31 -0300

    Python

--- Python/ex065.py
+++ Python/ex065.py
@@ -4,18 +4,17 @@ os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar
 continuar a digitar valores.
 """
 print('Digite quantos números inteiros quiser, e no final lhe mostrarei qual foi o maior valor, o menor e a média entre todos eles')
-resp = 'sim'
+resp = 's'
 valor = 0
 c = 0
 list = []
 
 
-while resp == 'sim':
+while resp == 's':
     x = int(input('Digite um valor: '))
     valor = valor + x
     c += 1
     list += [x]
-    resp = str(input('Quer continuar digitando mais valores? [sim/não]\n')).strip().lower()
+    resp = str(input('Quer continuar digitando mais valores? [sim/não]\n')).strip().lower()[0]
     media = valor / c
-
-print('A média entre todos os valores lidos foi de {}. O maior valor digitado foi de {}. E o menor valor digitado foi de {}.'.format(media, max(list), min(list)))
+print(f'A média entre todos os valores lidos foi de {media:.1f}. O maior valor digitado foi de {max(list)}. E o menor valor digitado foi de {min(list)}.')
