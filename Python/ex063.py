85547274075dd2fdef12a85fb52dcdadca232fc8
Marcos Ribas <74434215+MarcosRibas@users.noreply.github.com>
2021-01-12 23:36:25 -0300

    Python

--- Python/ex063.py
+++ Python/ex063.py
@@ -1,5 +1,5 @@
 """
-Ex063 Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n primeiros elementos de uma
+Ex063 Escreva um programa que leia um número inteiro qualquer e mostre na tela os primeiros elementos de uma
 sequência Fibonacci.
 """
 print('SEQUÊNCIA DE FIBONACCI')
