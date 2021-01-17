b9b0fd796fb7eee21d68dd84044127ffee9abe84
Marcos Ribas <contatomarcosribas@gmail.com>
2021-01-17 15:00:45 -0300

    Python

---
+++ Python/ex074.py
@@ -0,0 +1,3 @@
+"""Ex074 Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
+Depois disso, mostre a listagem de números geradsos s também indique o menor e o maior valor que estão na tupla
+"""
---
+++ Python/ex077.py
@@ -0,0 +1,4 @@
+"""
+Ex077 Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar,
+para cada palavra, quais são suas vogais.
+"""
