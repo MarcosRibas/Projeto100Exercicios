64869bb3a7455680c431456901894bfbdc8f08a3
Marcos Ribas <contatomarcosribas@gmail.com>
2021-01-17 14:43:44 -0300

    Python

---
+++ Python/ex072.py
@@ -0,0 +1,4 @@
+'''
+Ex072 Crie um programa que tenha uma tupla totalmente preenchida com contagem por extensão, de zero até vinte.
+Seu programa deverá ler pelo teclado (entre 0 e 20) e mostra-lo por extenso.
+'''
---
+++ Python/ex073.py
@@ -0,0 +1,9 @@
+'''
+Seu programa deverá ler pelo teclado (entre 0 e 20) e mostra-lo por extenso.
+Ex073 Crie uma tupla preenchida com os 20 primeiros colocados da tabela do campeonato brasileiro de futebol, na ordem
+de colocação. Depois mostre:
+a)	Apenas os 5 primeiros colocados
+b)	Os últimos quatro colocados
+c)	Uma lista com os times em ordem alfabética.
+d)	Em que posição da tabela está o time Chapecoense.
+'''
