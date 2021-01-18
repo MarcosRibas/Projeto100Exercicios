f8e3a909d5260c9a7f7b8bf2df84c7fc6e14a40f
Marcos Ribas <contatomarcosribas@gmail.com>
2021-01-18 19:30:22 -0300

    Python

--- Python/ex072.py
+++ Python/ex072.py
@@ -2,3 +2,7 @@
 Ex072 Crie um programa que tenha uma tupla totalmente preenchida com contagem por extensão, de zero até vinte.
 Seu programa deverá ler pelo teclado (entre 0 e 20) e mostra-lo por extenso.
 '''
+ext = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze',
+       'catorze', 'quinze', 'dezeseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+num = int(input('Digite um número de 1 a 20: '))
+print(ext[num])
--- Python/ex073.py
+++ Python/ex073.py
@@ -5,5 +5,19 @@ de colocação. Depois mostre:
 a)	Apenas os 5 primeiros colocados
 b)	Os últimos quatro colocados
 c)	Uma lista com os times em ordem alfabética.
-d)	Em que posição da tabela está o time Chapecoense.
+d)	Em que posição da tabela está o time Flamengo.
 '''
+tabela = ('São Paulo', 'Internacional', 'Atlético-MG', 'Grêmio', 'Flamengo', 'Palmeiras', 'Fluminense', 'Santos', \
+         'Corinthians', 'Athletico','Ceará', 'Bragantino', 'Atlético- GO', 'Sport', 'Vasco', 'Fortaleza', 'Bahia', \
+         'Goiás', 'Coritiba', 'Botafogo')
+print('-'*20)
+print('Os times que estão se classificando para a libertadores:')
+for c in (tabela[:5]):
+    print(c)
+print('-'*20)
+print('Os times que estão na zona de rebaixamento: ')
+for c in (tabela[16:20]):
+    print(c)
+print('Tabela em ordem alfabética: \n',(sorted(tabela)))
+fla = (tabela.index('Flamengo'))+1
+print(f'O Flamengo está na {fla}° posição')
