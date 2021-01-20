23ec01415df349b61f61564474b1f2c85ad99367
Marcos Ribas <contatomarcosribas@gmail.com>
2021-01-20 19:43:51 -0300

    Python

--- Python/ex074.py
+++ Python/ex074.py
@@ -1,13 +1,10 @@
 """Ex074 Crie um programa que vai gerar cinco números aleatórios e colocar em uma tupla.
 Depois disso, mostre a listagem de números gerados s também indique o menor e o maior valor que estão na tupla
 """
-import random
-s = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-x = []
-for c in range(0,5):
-    num = random.choice(s)
-    x = num
-print(x)
+from random import randint
+s = (randint(1,10), randint(1,10), randint(1,10), randint(1, 10), randint(1, 10))
+print(f'Números sorteados: {s}')
+print(f'O maior número sorteado foi {max(s)}, e o menor foi {min(s)}')
 
 
 
