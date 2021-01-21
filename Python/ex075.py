4dc65163d5714806590167b91d7d9be6845ffbda
Marcos Ribas <contatomarcosribas@gmail.com>
2021-01-21 11:42:01 -0300

    Python

--- Python/ex075.py
+++ Python/ex075.py
@@ -5,7 +5,16 @@ b) Em que posição foi digitado o primeiro valor 3.
 c) Quais foram os números pares.
 
 """
-num = ()
-for c in range(1, 5):
-    num = int(input(f'Digite o {c}° valor: '))
-print(num)
+num = (int(input('Digite um número: ')), int(input('Digite outro número: ')), int(input('Digite outro número: ')),
+       int(input('Digite o ultimo número: ')))
+print(f'Você digitou os números {num}')
+print(f'Você digitou o número 9, {num.count(9)} vez(es)')
+if 3 in num:
+    print(f'O número 3 foi digitado pela primeira vez na posição {num.index(3)+1}')
+else:
+    print('O número 3 não foi digitado')
+print('Os números pares digitados foram: ')
+for c in num:
+    if c % 2 == 0:
+        print(c)
+
--- Python/ex077.py
+++ Python/ex077.py
@@ -2,3 +2,9 @@
 Ex077 Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar,
 para cada palavra, quais são suas vogais.
 """
+palavras = ('Brasil', 'Sol', 'Praia', 'Feijoada', 'Carnaval', 'Caipirinha', 'Futebol', 'Natureza')
+for c in palavras:
+    print(f'Na palavra {c.upper()} temos as vogais: ')
+    for n in c:
+        if n in 'aeiou':
+            print(n)
