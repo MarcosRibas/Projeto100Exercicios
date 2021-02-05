1ef4bc8f942e4ef41142f26b6a1de79cbe5681df
Marcos Ribas <contatomarcosribas@gmail.com>
2021-02-05 15:57:39 -0300

    Python

--- Python/ex110/princial.py
+++ Python/ex110/princial.py
@@ -3,7 +3,5 @@ resumo(), que mostre na tela algumas informações geradas pelas funções que j
 from ex110 import moeda
 
 preço = float(input('Digite o preço: R$'))
-print(f'A metade de {moeda.moeda(preço)} é {moeda.metade(preço, True)}')
-print(f'O dobro de {moeda.moeda(preço)} é {moeda.dobro(preço, True)}')
-print(f'Aumentando 10%, temos {moeda.aumentar(preço, 10, True)}')
-print(f'Diminuido 10%, temos {moeda.diminuir(preço, 10, True)}')
+moeda.resumo(preço)
+
