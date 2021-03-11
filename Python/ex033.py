cf65d5c25d90c477711b3171c2c6ce48835cc9df
Marcos Ribas <74434215+MarcosRibas@users.noreply.github.com>
2021-03-10 23:08:48 -0300

    Python

--- Python/ex033.py
+++ Python/ex033.py
@@ -1,4 +1,4 @@
-#Ex033 Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+#Ex033 Faça um programa que leia três números e mostre qual é o maior, e qual é o menor.
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
 n3 = int(input('Digite o ultimo número: '))
